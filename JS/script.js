5832c3ea5b31d96c8cd6fbe2f721fc8c55eb2531
Allison Williamson <allisonmaree@gmail.com>
2017-03-05 12:22:43 -0800

    updated JSON and JSCRIPT

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,97 @@
-$( document ).ready(function() {
-    $.getJSON("http://localhost:8888/KellyForslund.com/JS/JSON/data.json", function(data){
-        console.log(data);
-    }); 
-});
+$(document).ready(function() {
+
+	var hot=[];
+
+$("#button_tables").click(function(){
+
+	$.getJSON('/KellyForslund.com/JS/JSON/data.json', function(data){
+
+		 hot = data;
+
+		console.log("You want to search for tables.");
+
+	});
+
+ });
+
+
+$("#button_dining").click(function() {
+
+	for (i=0; i < hot.tables.length; i++) {
+
+		  if($.inArray("dining", hot.tables[i].type)) {
+
+		  	console.log(hot.tables[i])
+
+		  	 } else {
+
+		  		delete hot.tables[i];
+				
+			}
+			
+		} 
 	
-    // $.ajax({
-    //     dataType: 'json',
-    //     beforeSend : function() {
-    //        console.log('Before Ajax Request Starts !!');
-    //     },
-    //     success: function(data) {
-    //         console.log(data);
-    //         alert(data);
-    //         document.write(data);
-    //     },
-    //     error : function(jqXHR, textStatus, errorThrown) {
-    //          alert("Error occurred: " + errorThrown);
-    //     },
-    //      afterSend : function() {
-    //        console.log('Ajax Request Complete  !!');
-    //     },
-    //     url: 'localhost8080/KellyForslund.com/JS/JSON/data.json'
-    // });  
+
+		console.log(hot.tables.length);
+
+});
+
+$("#button_coffee").click(function() {
+
+	for (i=0; i < hot.tables.length; i++) {
+
+		if(hot.tables[i].type !== "coffee") {
+			
+			delete hot.tables[i];
+			
+		} 
+		}
+
+		console.log(hot.tables.length);
+
+});
 
 
- 
+$("#button_side").click(function() {
+
+	for (i=0; i < hot.tables.length; i++) {
+
+		if(hot.tables[i].type !== "side") {
+			
+			delete hot.tables[i];
+			
+		} 
+		}
+
+		console.log(hot.tables.length);
+
+});
+
+ });
+
+
+		// for (i = 0; i < obj.tables.length; i++) {
+
+		//  	if(obj.tables[i].shape == "round") {
+
+		//  	 	hot.push(obj.tables[i]);
+
+		//  	 		 } else {
+
+		//  	 			alert("sorry there is nothing here") }
+		//  	 		}
+		 	 	
+
+		//  	console.log(hot);
+
+		
+		
+	
+    	
+    	
+
+   
+    
+
+	
+   
