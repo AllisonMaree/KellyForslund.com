9177cbf2dda119f619bc67b02033d4dfe784a68c
matt_wilson_shopperTrak <matt_wilson_shopperTrak@bitbucket.org>
2017-03-19 13:58:38 -0500

    selecting user options

--- JS/controllers.js
+++ JS/controllers.js
@@ -1,10 +1,22 @@
  var app = angular.module('myApp', []);
 
  app.controller('myController', function($scope, $http) {
+       
+       var dataaray = [];
+
+       var dataObj = {}
+
 
  		var onDataComplete = function(response){
+
+ 			alert("11");
+ 			
+ 			$scope.selections = dataaray;
+
+ 			$scope.objSelections =dataObj;
+
  			$scope.myData = response.data; 		
- 			$http.get($scope.myData.key);	
+ 			//$http.get($scope.myData.key);	
  		};
 
  		var onResponse = function(response){
@@ -17,6 +29,34 @@
  		}
 
  		$scope.search = function(data) {
+          
+              dataObj.material = {
+                  "Wood" : $("#matWood").is(':checked'),
+                  "Metal":$("#matMetal").is(':checked'),
+                  "Other" :$("#matOther").is(':checked')
+              }
+
+              dataObj.style = {
+                  "Traditional" : $("#styTraditional").is(':checked'),
+                  "Transitional":$("#styTransitional").is(':checked'),
+                  "Modern" :$("#styModern").is(':checked'),
+                  "Outdoor" :$("#styOutdoor").is(':checked')
+              }
+
+              
+               var matWood =  $("#matWood").is(':checked');
+               var matMetal =  $("#matMetal").is(':checked');
+               var matOther =  $("#matOther").is(':checked');
+
+               var styTraditional =  $("#styTraditional").is(':checked');
+               var styTransitional =  $("#styTransitional").is(':checked');
+               var styModern =  $("#styModern").is(':checked');
+               var styOutdoor =  $("#styOutdoor").is(':checked');
+             
+             dataaray.push([matWood,matMetal,matOther])
+             
+             dataaray.push([styTraditional,styTransitional,styModern,styOutdoor])
+
  			 $http.get("/KellyForslund.com/JS/JSON/data.json")
  			 .then(onDataComplete, onError);
  		};
