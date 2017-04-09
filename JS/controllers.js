dc946432c07b94a9c36b4fcf6046b30092298392
Allison Williamson <allisonmaree@gmail.com>
2017-04-09 11:39:35 -0700

    working on iteration

--- JS/controllers.js
+++ JS/controllers.js
@@ -1,26 +1,75 @@
  var app = angular.module('myApp', []);
 
  app.controller('myController', function($scope, $http) {
-       
-       var dataaray = [];
 
-       var dataObj = {}
+       var isCheckedObj = {}
+       var masterList = []
+
+
+// CREATE MASTER BOOLEAN lIST OF PARAMETERS //
+
+		var isChecked = function() {
+
+   			isCheckedObj.material = {
+                  "Wood" : $("#matWood").is(':checked'),
+                  "Metal":$("#matMetal").is(':checked'),
+                  "Other" :$("#matOther").is(':checked')
+              }
+
+              isCheckedObj.style = {
+                  "Traditional" : $("#styTraditional").is(':checked'),
+                  "Transitional":$("#styTransitional").is(':checked'),
+                  "Modern":$("#styModern").is(':checked'),
+                  "Outdoor":$("#styOutdoor").is(':checked')
+              }
+               isCheckedObj.size = {
+                  "small" :$("#sizeSmall").is(':checked'),
+                  "medium":$("#sizeMedium").is(':checked'),
+                  "large" :$("#sizeLarge").is(':checked'),
+              }
+                isCheckedObj.leadtime = {
+                  "quickship":$("#leadtimeQuickship").is(':checked'),
+                  "standard":$("#leadtimeStandard").is(':checked'),
+              }
+
+           };
+
+
+// CREATE LIST OF PARAMETERS = "TRUE" //
+
+
+		var customerPreference = function() {
+
+			isChecked();
+
+			var objLength = Object.keys(isCheckedObj).length;
+
+			console.log("Initial object: " + isCheckedObj);
+			
+			for(i = 0; i < objLength; i++){
+
+				Object.keys(isCheckedObj).forEach(function(key) {
+					if (!isCheckedObj[key]) delete isCheckedObj[key];
+				});
+
+				// console.log(Object.keys(isCheckedObj));
+
+				console.log("edited object: " + isCheckedObj);			
+     		 	
+}
+}
+
+			//console.log(Object.values(masterList));
+
 
 
  		var onDataComplete = function(response){
 
- 			alert("11");
- 			
- 			$scope.selections = dataaray;
-
- 			$scope.objSelections =dataObj;
-
  			$scope.myData = response.data; 		
- 			//$http.get($scope.myData.key);	
+ 	
  		};
 
  		var onResponse = function(response){
- 			//$scope.response = response.data;
  			console.log("onResponse works");
  		};
 
@@ -29,39 +78,16 @@
  		}
 
  		$scope.search = function(data) {
-          
-              dataObj.material = {
-                  "Wood" : $("#matWood").is(':checked'),
-                  "Metal":$("#matMetal").is(':checked'),
-                  "Other" :$("#matOther").is(':checked')
-              }
 
-              dataObj.style = {
-                  "Traditional" : $("#styTraditional").is(':checked'),
-                  "Transitional":$("#styTransitional").is(':checked'),
-                  "Modern" :$("#styModern").is(':checked'),
-                  "Outdoor" :$("#styOutdoor").is(':checked')
-              }
-
-              
-               var matWood =  $("#matWood").is(':checked');
-               var matMetal =  $("#matMetal").is(':checked');
-               var matOther =  $("#matOther").is(':checked');
-
-               var styTraditional =  $("#styTraditional").is(':checked');
-               var styTransitional =  $("#styTransitional").is(':checked');
-               var styModern =  $("#styModern").is(':checked');
-               var styOutdoor =  $("#styOutdoor").is(':checked');
-             
-             dataaray.push([matWood,matMetal,matOther])
-             
-             dataaray.push([styTraditional,styTransitional,styModern,styOutdoor])
-
- 			 $http.get("/KellyForslund.com/JS/JSON/data.json")
+          	$http.get("/KellyForslund.com/JS/JSON/data.json")
  			 .then(onDataComplete, onError);
+
+ 			 customerPreference();
+
  		};
 
 	});
+
 
 
 	
